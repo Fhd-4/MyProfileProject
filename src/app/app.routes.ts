d029144ffe2dceb77cb2fa88762e1a2fa8f51a5d
Fhd-4 <fahadold91@gmail.com>
2026-07-21 16:52:34 +0300

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login.component';
+import { Dashboard } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  // Future component routes to be attached by your teammate:
-  // { path: 'card', component: CardComponent },
-  // { path: 'super-admin', component: SuperAdminComponent }
+  { path: 'dashboard', component: Dashboard }
 ];
