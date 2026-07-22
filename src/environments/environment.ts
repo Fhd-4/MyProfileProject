309c1c3ce6d4cc8d021d9548822b994075873a39
Fhd-4 <fahadold91@gmail.com>
2026-07-22 16:05:04 +0300

    co

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // Please replace with your actual production MonsterASP backend domain URL, e.g., 'https://your-api.monsterasp.net/api'
-  apiUrl: 'http://fahd1.runasp.net/swagger/index.html' 
+  apiUrl: 'https://fahd1.runasp.net/api'
 };
