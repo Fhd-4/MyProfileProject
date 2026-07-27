9ca3f4a307641c82888108c59921de59a17650fb
Fhd-4 <fahadold91@gmail.com>
2026-07-27 09:33:23 +0300

    Change user-card renderMode to Prerender to generate static HTML file

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,7 +3,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'user-card',
-    renderMode: RenderMode.Client
+    renderMode: RenderMode.Prerender
   },
   {
     path: 'user-card/:id',
